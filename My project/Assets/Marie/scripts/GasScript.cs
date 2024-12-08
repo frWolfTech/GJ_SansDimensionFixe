08d54a926f77174f042b064254ee1c5209f84764
William's Student <102622446+WilliamChapron@users.noreply.github.com>
2024-12-08 13:39:11 +0100

    d
    d

--- My project/Assets/Marie/scripts/GasScript.cs
+++ My project/Assets/Marie/scripts/GasScript.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  // Nécessaire pour travailler avec l'UI dans Unity
 
 public class GasScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float gasPercent = 1f; // Valeur initiale de gaz (100%)
+
+    public Slider gasBar; // Référence à l'UI Image qui représente la barre de gaz
+
+    private void Awake()
     {
-        
+        InvokeRepeating(nameof(Loose1PercentGas), 0.5f, 0.5f); // Réduire le gaz toutes les 0.5 secondes
     }
 
-    // Update is called once per frame
+    private void Loose1PercentGas()
+    {
+        gasPercent -= 0.1f; // Réduit de 1% (0.01)
+
+        if (gasPercent < 0f)
+        {
+            gasPercent = 0f; // Empêcher le gaz de devenir négatif
+        }
+    }
+
     void Update()
     {
-        
+        gasBar.fillAmount = gasPercent;
+
+        Debug.Log("Gas Percent: " + gasPercent);
     }
 }
